310226fcebe99c5146498371c730706fe5e9587e
ZachRC <zacharyrcherney@gmail.com>
2024-09-27 04:23:35 -0500

    smh

--- myproject/settings.py
+++ myproject/settings.py
@@ -136,6 +136,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
